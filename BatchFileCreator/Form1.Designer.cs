4f2455db994d720f24e5b988478ebf90941ec3d8
Georgiy <149246960+neomitt@users.noreply.github.com>
2024-07-26 13:48:06 +0500

    Add files via upload

---
+++ BatchFileCreator/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace BatchFileCreator
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.textBoxPath5 = new System.Windows.Forms.TextBox();
+            this.textBoxPath3 = new System.Windows.Forms.TextBox();
+            this.textBoxPath4 = new System.Windows.Forms.TextBox();
+            this.textBoxPath0 = new System.Windows.Forms.TextBox();
+            this.textBoxPath6 = new System.Windows.Forms.TextBox();
+            this.textBoxPath7 = new System.Windows.Forms.TextBox();
+            this.textBoxPath8 = new System.Windows.Forms.TextBox();
+            this.textBoxPath1 = new System.Windows.Forms.TextBox();
+            this.textBoxPath2 = new System.Windows.Forms.TextBox();
+            this.textBoxPath9 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.GenerateButton.Location = new System.Drawing.Point(53, 458);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(300, 35);
+            this.GenerateButton.TabIndex = 0;
+            this.GenerateButton.Text = "Create";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // textBoxPath5
+            // 
+            this.textBoxPath5.Location = new System.Drawing.Point(53, 253);
+            this.textBoxPath5.Name = "textBoxPath5";
+            this.textBoxPath5.Size = new System.Drawing.Size(300, 20);
+            this.textBoxPath5.TabIndex = 1;
+            // 
+            // textBoxPath3
+            // 
+            this.textBoxPath3.Location = new System.Drawing.Point(53, 171);
+            this.textBoxPath3.Name = "textBoxPath3";
+            this.textBoxPath3.Size = new System.Drawing.Size(300, 20);
+            this.textBoxPath3.TabIndex = 2;
+            // 
+            // textBoxPath4
+            // 
+            this.textBoxPath4.Location = new System.Drawing.Point(53, 206);
+            this.textBoxPath4.Name = "textBoxPath4";
+            this.textBoxPath4.Size = new System.Drawing.Size(300, 20);
+            this.textBoxPath4.TabIndex = 3;
+            // 
+            // textBoxPath0
+            // 
+            this.textBoxPath0.Location = new System.Drawing.Point(53, 42);
+            this.textBoxPath0.Name = "textBoxPath0";
+            this.textBoxPath0.Size = new System.Drawing.Size(300, 20);
+            this.textBoxPath0.TabIndex = 4;
+            // 
+            // textBoxPath6
+            // 
+            this.textBoxPath6.Location = new System.Drawing.Point(53, 294);
+            this.textBoxPath6.Name = "textBoxPath6";
+            this.textBoxPath6.Size = new System.Drawing.Size(300, 20);
+            this.textBoxPath6.TabIndex = 4;
+            // 
+            // textBoxPath7
+            // 
+            this.textBoxPath7.Location = new System.Drawing.Point(53, 337);
+            this.textBoxPath7.Name = "textBoxPath7";
+            this.textBoxPath7.Size = new System.Drawing.Size(300, 20);
+            this.textBoxPath7.TabIndex = 5;
+            // 
+            // textBoxPath8
+            // 
+            this.textBoxPath8.Location = new System.Drawing.Point(53, 379);
+            this.textBoxPath8.Name = "textBoxPath8";
+            this.textBoxPath8.Size = new System.Drawing.Size(300, 20);
+            this.textBoxPath8.TabIndex = 6;
+            // 
+            // textBoxPath1
+            // 
+            this.textBoxPath1.Location = new System.Drawing.Point(53, 85);
+            this.textBoxPath1.Name = "textBoxPath1";
+            this.textBoxPath1.Size = new System.Drawing.Size(300, 20);
+            this.textBoxPath1.TabIndex = 7;
+            // 
+            // textBoxPath2
+            // 
+            this.textBoxPath2.Location = new System.Drawing.Point(53, 130);
+            this.textBoxPath2.Name = "textBoxPath2";
+            this.textBoxPath2.Size = new System.Drawing.Size(300, 20);
+            this.textBoxPath2.TabIndex = 8;
+            // 
+            // textBoxPath9
+            // 
+            this.textBoxPath9.Location = new System.Drawing.Point(53, 418);
+            this.textBoxPath9.Name = "textBoxPath9";
+            this.textBoxPath9.Size = new System.Drawing.Size(300, 20);
+            this.textBoxPath9.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Path 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Path 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Path 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Path 4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Path 5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 256);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Path 6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 297);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Path 7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 340);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Path 8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 382);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(38, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Path 9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 421);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 13);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Path 10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.label11.Location = new System.Drawing.Point(50, 10);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(309, 17);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Enter the paths for the batch file creation";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(384, 511);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPath9);
+            this.Controls.Add(this.textBoxPath2);
+            this.Controls.Add(this.textBoxPath1);
+            this.Controls.Add(this.textBoxPath8);
+            this.Controls.Add(this.textBoxPath7);
+            this.Controls.Add(this.textBoxPath6);
+            this.Controls.Add(this.textBoxPath0);
+            this.Controls.Add(this.textBoxPath4);
+            this.Controls.Add(this.textBoxPath3);
+            this.Controls.Add(this.textBoxPath5);
+            this.Controls.Add(this.GenerateButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Batch File Creator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GenerateButton;
+        private System.Windows.Forms.TextBox textBoxPath5;
+        private System.Windows.Forms.TextBox textBoxPath3;
+        private System.Windows.Forms.TextBox textBoxPath4;
+        private System.Windows.Forms.TextBox textBoxPath0;
+        private System.Windows.Forms.TextBox textBoxPath6;
+        private System.Windows.Forms.TextBox textBoxPath7;
+        private System.Windows.Forms.TextBox textBoxPath8;
+        private System.Windows.Forms.TextBox textBoxPath1;
+        private System.Windows.Forms.TextBox textBoxPath2;
+        private System.Windows.Forms.TextBox textBoxPath9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
