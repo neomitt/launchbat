7cd3901ef51ef452df6ae7abeb2a8dc1098756e2
Georgiy <149246960+neomitt@users.noreply.github.com>
2024-07-26 12:47:16 +0500

    Add files via upload

---
+++ BatchFileCreator/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BatchFileCreator
+{
+    public partial class Form1 : Form
+    {
+        private const int MaxApplications = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            string batFilePath = "launch_apps.bat";
+            using (StreamWriter writer = new StreamWriter(batFilePath))
+            {
+                for (int i = 0; i < MaxApplications; i++)
+                {
+                    TextBox textBox = this.Controls["textBoxPath" + i] as TextBox;
+                    if (!string.IsNullOrEmpty(textBox?.Text))
+                    {
+                        writer.WriteLine($"start \"\" \"{textBox.Text}\"");
+                    }
+                }
+            }
+
+            MessageBox.Show($"Batch file created: {batFilePath}");
+        }
+    }
+}
---
+++ BatchFileCreator/bin/Debug/launch_apps.bat
@@ -0,0 +1,1 @@
+start "" "C:\Users\koval\Desktop\Надо"
